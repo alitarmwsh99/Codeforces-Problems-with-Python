1b80b21e24077f7c23a952f8cf783913e8d5ed1d
Ali Ahmed Tarmoush <78211183+alitarmwsh99@users.noreply.github.com>
2021-04-20 21:13:34 +0300

    Add files via upload

---
+++ Watermelon.py
@@ -0,0 +1,11 @@
+#Problem Link   : https://codeforces.com/problemset/problem/4/A
+#Problem Name   : Watermelon
+#Language:      :Python 3.9.4
+#Programmer     : Ali Ahmed Tarmoush
+#Status         : Accepted
+w=input()
+w=int(w)
+if w % 2 == 0 and w != 2 :
+    print('YES')
+else :
+    print('NO')
